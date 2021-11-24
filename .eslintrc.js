d4e5543d9d5b177b3db33b0170872bb1342d138e
omar sobhy <omar.ahmed.sobhy.96@gmail.com>
2021-11-24 17:08:16 +0400

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    'extends': [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended'
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 13,
+        'sourceType': 'module',
+        'project': './tsconfig.json'
     },
-    "plugins": [
-        "@typescript-eslint"
+    'plugins': [
+        '@typescript-eslint'
     ],
-    "rules": {
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        'no-console': ['off'],
+        '@typescript-eslint/no-use-before-define': ['off']
+        // 'import/no-cycle': ['off']
     }
 };
