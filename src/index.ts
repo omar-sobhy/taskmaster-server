b0bf1d3a17e46bdfc5df3e950491bf6b44b2eed3
omar sobhy <omar.ahmed.sobhy.96@gmail.com>
2021-11-28 16:44:10 +0400

    Updated init code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,59 @@
+import 'reflect-metadata';
+
 import express from 'express';
 
-const app = express()
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import initDb from './database';
+import errorMiddleware from './middleware/error.middleware';
+import loggerMiddleware from './middleware/logger.middleware';
+import UserRoutes from './routes/User.routes';
+import ProjectRoutes from './routes/Project.routes';
+import SectionRoutes from './routes/Section.routes';
 
-const port = 3000
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const routes = [new UserRoutes(), new ProjectRoutes(), new SectionRoutes()];
+
+async function start() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(cookieParser());
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  app.use(loggerMiddleware);
+
+  routes.forEach((r) => {
+    app.use(r.router);
+  });
+
+  app.use(errorMiddleware);
+
+  try {
+    await connectToDb();
+    console.log('Connected to db successfully');
+
+    const port = 3000;
+
+    app.listen(port, () => {
+      console.log(`Listening at port ${port}`);
+    });
+  } catch (error) {
+    console.error('An error was thrown while starting. Exiting...');
+  }
+}
+
+async function connectToDb() {
+  try {
+    await initDb();
+  } catch (error) {
+    console.error('Error thrown while connecting to db');
+    console.error(error);
+    throw error;
+  }
+}
+
+start();
