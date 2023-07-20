742cce6f59fbe819dcfa42e0278c6f618c6db672
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2023-07-20 21:39:28 +0400

    Move Task.database.functions to Task.controller

--- src/controllers/Task.controllers.ts
+++ src/controllers/Task.controllers.ts
@@ -1,44 +1,24 @@
+import * as mongoose from 'mongoose';
+import { Types } from 'mongoose';
+
 import ProjectModel from '../database/Project/Project.model';
 import SectionModel from '../database/Section/Section.model';
 import Task from '../database/Task/Task.interface';
 import TaskModel from '../database/Task/Task.model';
 import Result from '../interfaces/Result';
+import Comment from '../database/Comment/Comment.interface';
+import CommentModel from '../database/Comment/Comment.model';
+import TagModel from '../database/Tag/Tag.model';
 
-async function createTask(
-  sectionId: string,
-  name: string,
-  dueDate?: Date,
-  assignee?: string,
-): Promise<Result<Task, 'SECTION_NOT_FOUND' | 'USER_NOT_IN_PROJECT'>> {
+async function getTask(taskId: string): Promise<Result<Task, 'TASK_NOT_FOUND'>> {
   try {
-    const section = await SectionModel.findById(sectionId);
-
-    if (!section) {
+    const task = await TaskModel.findById(taskId, '-__v');
+    if (!task) {
       return {
         type: 'error',
-        errorType: 'SECTION_NOT_FOUND',
+        errorType: 'TASK_NOT_FOUND',
       };
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const project = (await ProjectModel.findById(section.project))!;
-
-    const user = project.users.find((u) => u.toString() === assignee);
-    if (!user) {
-      return {
-        type: 'error',
-        errorType: 'USER_NOT_IN_PROJECT',
-      };
-    }
-
-    const task = await new TaskModel({
-      name,
-      dueDate,
-      assignee,
-      created: new Date(),
-      updated: new Date(),
-      section: section._id,
-    }).save();
 
     return {
       type: 'success',
@@ -47,13 +27,164 @@ async function createTask(
   } catch (error) {
     return {
       type: 'error',
-      errorType: 'SECTION_NOT_FOUND',
+      errorType: 'TASK_NOT_FOUND',
     };
   }
 }
 
-async function deleteTask(_userId: string, _taksId: string) {
-  // TODO
+async function updateTask(
+  taskId: string,
+  {
+    name,
+    dueDate,
+    assignee,
+    description,
+    tags,
+  }: Partial<Task>,
+): Promise<Result<Task, 'TASK_NOT_FOUND' | 'ASSIGNEE_NOT_IN_PROJECT' | 'TAG_NOT_FOUND'>> {
+  try {
+    const task = await TaskModel.findById(taskId, '-__v');
+    if (!task) {
+      return {
+        type: 'error',
+        errorType: 'TASK_NOT_FOUND',
+      };
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const section = (await SectionModel.findById(task.section))!;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const project = (await ProjectModel.findById(section.project))!;
+
+    if (tags) {
+      const tagPromises = await Promise.all(tags.map((t) => TagModel.findById(t)));
+
+      const invalidTag = tagPromises.find((t) => {
+        if (!t) return true;
+
+        if (t.project._id.toString() !== project.id) {
+          console.log(`Tried to assign tag ${t.id} to non-parent project ${project.id}.`);
+          return true;
+        }
+
+        return false;
+      });
+
+      if (invalidTag) {
+        return {
+          type: 'error',
+          errorType: 'TAG_NOT_FOUND',
+          errorData: invalidTag.id,
+        };
+      }
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      task.tags = tags as mongoose.ObjectId;
+    }
+
+    if (name) task.name = name;
+
+    try {
+      if (assignee === null) {
+        task.assignee = undefined;
+      } else if (assignee !== undefined) {
+        task.assignee = new Types.ObjectId(assignee);
+      }
+    } catch (error) {
+      return {
+        type: 'error',
+        errorType: 'ASSIGNEE_NOT_IN_PROJECT',
+        errorData: project._id.toString(),
+      };
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (dueDate === null || dueDate === '') task.dueDate = undefined;
+    else if (dueDate) task.dueDate = new Date(dueDate);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (description === null || description === '') task.description = undefined;
+    else if (description) task.description = description;
+
+    task.updated = new Date();
+
+    await task.save();
+    return {
+      type: 'success',
+      data: task,
+    };
+  } catch (error) {
+    return {
+      type: 'error',
+      errorType: 'TASK_NOT_FOUND',
+    };
+  }
 }
 
-export { createTask, deleteTask };
+async function deleteTask(taskId: string): Promise<Result<Task, 'TASK_NOT_FOUND'>> {
+  try {
+    const task = await TaskModel.findByIdAndDelete(taskId);
+    if (!task) {
+      return {
+        type: 'error',
+        errorType: 'TASK_NOT_FOUND',
+      };
+    }
+
+    return {
+      type: 'success',
+      data: task,
+    };
+  } catch (error) {
+    return {
+      type: 'error',
+      errorType: 'TASK_NOT_FOUND',
+    };
+  }
+}
+
+async function createComment(
+  taskId: string,
+  comment: string,
+  owner: string,
+): Promise<Result<Comment, 'TASK_NOT_FOUND'>> {
+  try {
+    const task = await TaskModel.findById(taskId);
+    if (!task) {
+      return {
+        type: 'error',
+        errorType: 'TASK_NOT_FOUND',
+      };
+    }
+
+    const newComment = new CommentModel({
+      text: comment,
+      date: new Date(),
+      task: taskId,
+      owner,
+    });
+
+    await newComment.save();
+
+    task.comments.push(newComment.id);
+
+    await task.save();
+    return {
+      type: 'success',
+      data: newComment,
+    };
+  } catch (error) {
+    return {
+      type: 'error',
+      errorType: 'TASK_NOT_FOUND',
+    };
+  }
+}
+
+export {
+  deleteTask, updateTask, getTask, createComment,
+};
--- src/middleware/permission.middleware.ts
+++ src/middleware/permission.middleware.ts
@@ -1,48 +1,73 @@
 import { NextFunction, Request, Response } from 'express';
-import { getProject } from '../controllers/Project.controllers';
+import { getProject, getTags } from '../controllers/Project.controllers';
 import InsufficientPermissionsException from '../exceptions/permissions/InsufficientPermissionsException';
 import ProjectNotFoundException from '../exceptions/projects/ProjectNotFoundException';
 import RequestWithUser from '../interfaces/RequestWithUser.interface';
 import SectionNotFoundException from '../exceptions/sections/SectionNotFoundException';
 import { getSection } from '../controllers/Section.controllers';
+import { getTask } from '../controllers/Task.controllers';
+import TaskNotFoundException from '../exceptions/tasks/TaskNotFoundException';
 
 function projectPermissionMiddleware(type: 'get' | 'update' | 'create' | 'delete') {
   return async function checkPermissions(req: Request, _res: Response, next: NextFunction) {
     const { user } = req as RequestWithUser;
 
-    const projectResult = await getProject(req.params.projectId);
-    if (projectResult.type === 'error') {
-      next(new ProjectNotFoundException(req.params.projectId));
-      return;
-    }
+    if (type !== 'create') {
+      const projectResult = await getProject(req.params.projectId);
+      if (projectResult.type === 'error') {
+        next(new ProjectNotFoundException(req.params.projectId));
+        return;
+      }
 
-    const project = projectResult.data;
+      const project = projectResult.data;
 
-    if (type === 'get') {
       if (!project.users.map((u) => u.toString()).includes(user._id.toString())) {
         // TODO log
         next(new InsufficientPermissionsException(user, { type: 'project', _id: user._id }));
+        return;
       }
+
+      // TODO other types, expanded permission check
+
+      next();
     }
-
-    // TODO other types, expanded permission check
-
-    next();
   };
 }
 
 function sectionPermissionMiddleware(type: 'get' | 'update' | 'create' | 'delete') {
   return async function checkPermissions(req: Request, res: Response, next: NextFunction) {
-    const { sectionId } = req.params;
+    if (type !== 'create') {
+      const { sectionId } = req.params;
 
-    const sectionResult = await getSection(sectionId);
+      const sectionResult = await getSection(sectionId);
 
-    if (sectionResult.type === 'error') {
-      next(new SectionNotFoundException(sectionId));
+      if (sectionResult.type === 'error') {
+        next(new SectionNotFoundException(sectionId));
+        return;
+      }
+
+      req.params.sectionId = sectionId;
     }
 
-    return projectPermissionMiddleware(type)(req, res, next);
+    projectPermissionMiddleware(type)(req, res, next);
   };
 }
 
-export { projectPermissionMiddleware, sectionPermissionMiddleware };
+function taskPermissionMiddleware(type: 'get' | 'update' | 'create' | 'delete') {
+  return async function checkPermissions(req: Request, res: Response, next: NextFunction) {
+    if (type !== 'create') {
+      const { taskId } = req.params;
+
+      const taskResult = await getTask(taskId);
+
+      if (taskResult.type === 'error') {
+        next(new TaskNotFoundException(taskId));
+        return;
+      }
+    }
+
+    sectionPermissionMiddleware(type)(req, res, next);
+  };
+}
+
+export { projectPermissionMiddleware, sectionPermissionMiddleware, taskPermissionMiddleware };
