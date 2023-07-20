f8439327a04c93b7af0f01d537af39f90836228c
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2023-07-20 15:47:02 +0400

    Init section tests

---
+++ src/tests/feature/section/section.test.ts
@@ -0,0 +1,108 @@
+import {
+  beforeAll, beforeEach, describe, expect, test,
+} from '@jest/globals';
+import request, { SuperAgentStatic } from 'superagent';
+import { faker } from '@faker-js/faker';
+import User from '../../../database/User/User.interface';
+import { signUp } from '../../../controllers/User.controllers';
+import Project from '../../../database/Project/Project.interface';
+import { createProject, createSections } from '../../../controllers/Project.controllers';
+import { createTask } from '../../../controllers/Task.controllers';
+import Section from '../../../database/Section/Section.interface';
+
+describe('section', () => {
+  const useHttps = process.env.USE_HTTPS;
+  const taskmasterPort = process.env.TASKMASTER_PORT;
+
+  const basePath = `${useHttps ? 'https' : 'http'}://localhost:${taskmasterPort}`;
+
+  let agent: SuperAgentStatic;
+  let user: User;
+
+  beforeAll(async () => {
+    const username = faker.internet.userName();
+
+    const userResult = await signUp(username, 'password', 'cool@email.com');
+
+    expect(userResult).not.toBe(null);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    user = userResult!;
+  });
+
+  beforeEach(async () => {
+    agent = request
+      .agent()
+      .type('application/json')
+      .ok(() => true);
+
+    if (expect.getState().currentTestName !== 'missing auth') {
+      const loginResult = await agent
+        .post(`${basePath}/users/login`)
+        .send({ username: user.username, password: 'password' });
+
+      expect(loginResult.ok).toBe(true);
+    }
+  });
+
+  describe('get section by id', () => {
+    let project: Project;
+    let section: Section;
+
+    beforeAll(async () => {
+      const projectResult = await createProject(user._id.toString(), faker.word.noun());
+
+      expect(projectResult).not.toBeNull();
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      project = projectResult!;
+
+      const sectionResult = await createSections(project._id.toString(), [
+        { name: faker.word.noun(), colour: faker.color.rgb(), icon: '' },
+      ]);
+
+      expect(sectionResult).not.toBeNull();
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      [section] = sectionResult!;
+    });
+
+    test.each([
+      { tasks: [] },
+      { tasks: [{ name: faker.word.noun() }] },
+      { tasks: [{ name: faker.word.noun() }, { name: faker.word.noun() }] },
+    ])('has tasks', async ({ tasks }) => {
+      const promises = tasks.map(async (task) => {
+        const taskResult = await createTask(section._id.toString(), task.name);
+
+        return expect(taskResult).not.toBeNull();
+      });
+
+      await Promise.all(promises);
+
+      const p = agent.get(`${basePath}/sections/${section._id}`);
+
+      await expect(p).resolves.toHaveProperty('status', 200);
+      await expect(p).resolves.toHaveProperty('body.tasks');
+
+      tasks.forEach((task) => {
+        expect(p).resolves.toHaveProperty(
+          'body.tasks',
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: task.name,
+            }),
+          ]),
+        );
+      });
+    });
+  });
+
+  describe('invalid request', () => {
+    test('missing auth', async () => {});
+
+    test('invalid section id', async () => {});
+
+    test.skip('missing permission to see section', async () => {});
+  });
+});
