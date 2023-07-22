6d29d8a1fa2e84c8da73d714e7542349e731c36a
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2023-07-22 18:41:29 +0400

    Make validation message reporting recursive for nested objects

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -2,24 +2,46 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate, ValidationError } from 'class-validator';
 import * as express from 'express';
 
+interface Constraints {
+  property: string;
+  constraints: {
+    [type: string]: string;
+  };
+}
+
+function getConstraints(error: ValidationError, parents: string[] = []): Constraints[] {
+  if (!error.children || error.children.length === 0) {
+    return [
+      {
+        property: [...parents, error.property].join('.'),
+        constraints: error.constraints!,
+      },
+    ];
+  }
+
+  // eslint-disable-next-line arrow-body-style
+  const childConstraints = error.children.map((c) => {
+    return getConstraints(c, [...parents, error.property]);
+  });
+
+  return childConstraints.flat();
+}
+
 function validationMiddleware<T>(type: ClassConstructor<T>): express.RequestHandler {
   return (req, res, next) => {
     validate(plainToInstance(type, req.body) as unknown as object).then(
       (errors: ValidationError[]) => {
         if (errors.length > 0) {
-          const message = errors
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            .map((error: ValidationError) => Object.values(error.constraints!))
+          const allConstraints = errors.flatMap((e) => getConstraints(e));
+
+          const message = allConstraints
+            .map((c) => Object.values(c.constraints).join('\n'))
             .join('\n');
 
-          const propsErrors = errors.map((error) => {
-            const constraints = Object.keys(error.constraints!);
-
-            return {
-              property: error.property,
-              constraints,
-            };
-          });
+          const propsErrors = allConstraints.map((c) => ({
+            property: c.property,
+            constraints: Object.keys(c.constraints),
+          }));
 
           res
             .status(400)
